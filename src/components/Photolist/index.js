ab26fac347427c27b222da22af172d6ef3af09c1
josh mitchell <jshmtchll@gmail.com>
2021-06-25 13:06:21 -0500

    deployement

--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -129,12 +129,14 @@ const PhotoList = ({ category }) => {
   const toggleModal = (image, i) => {
     // current photo
     setCurrentPhoto({...image, index: i});
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+      <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
